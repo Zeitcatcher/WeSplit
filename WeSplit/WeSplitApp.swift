c9fc765723373378d3b94014bbaa5dd16c4027f8
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-02-27 13:24:54 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Arseniy Oksenoyt on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
